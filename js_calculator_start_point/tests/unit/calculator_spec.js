8421922e6ceb347df5fdf2a5b94c373b12d976a1
Christopher Pearson <cpearson3140@gmail.com>
2020-07-03 10:04:15 +0100

    populated calculator_spec file
    Signed-off-by: Christopher Pearson <cpearson3140@gmail.com>

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,36 +11,55 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
-  // it should add 1 and 4 to get 5
-  it(1 + 4 = 5, function(){
-    assert.equal(5)
-  })
+  it ('should add 1 and 4 to get 5', function() {
+  calculator.previousTotal = 4
+  calculator.add(1)
+  assert.equal(5, calculator.runningTotal)
+});
 
-  // it should subtract 4 from 7 to get 3
-  it(7 - 4 = 3, function(){
-    assert.equal(3)
-  })
+  it ('should subtract 4 from 7 to get 3', function() {
+  calculator.previousTotal = 7
+  calculator.subtract(4)
+  assert.equal(3, calculator.runningTotal)
+});
 
-  // it should multiply 3 by 5 to get 15
-  it(3 * 5 = 15, function(){
-    assert.equal(15)
-  })
+  it ('should multiply 3 by 5 to get 15', function() {
+    calculator.previousTotal = 3
+    calculator.multiply(5)
+    assert.equal(15, calculator.runningTotal)
+  });
 
-  // it should divide 21 by 7 to get 3
-  it(21 / 7 = 3, function(){
-    assert.equal(3)
-  })
+  it ('should divide 21 by 7 to get 3', function() {
+    calculator.previousTotal = 21
+    calculator.divide(7)
+    assert.equal(3, calculator.runningTotal)
+  });
 
-  // it should concatenate multiple number button clicks
-  it(
-    assert.equal()
-  })
-  // it should chain multiple operations together
-  it(
-    assert.equal()
-  })
-  // it should clear running total without affecting calculation
-  it(
-    assert.equal()
-  })
+  it ('should concatenate multiple number button clicks', function () {
+    calculator.numberClick(1)
+    calculator.numberClick(2)
+    calculator.numberClick(3)
+    assert.equal(123, calculator.runningTotal)
+  });
+
+  it ('should chain multiple operations together', function () {
+    calculator.numberClick(1)
+    calculator.operatorClick('+')
+    calculator.numberClick(2)
+    calculator.operatorClick('*')
+    calculator.numberClick(3)
+    calculator.operatorClick('=')
+    assert.equal(9, calculator.previousTotal)
+  });
+
+  it ('should clear running total without affecting calculation', function () {
+    calculator.numberClick(1)
+    calculator.operatorClick('+')
+    calculator.numberClick(2)
+    calculator.operatorClick('=')
+    calculator.clearClick()
+    assert.equal(calculator.previousTotal, 3);
+    assert.equal(calculator.runningTotal, 0)
+  });
+  
 });
